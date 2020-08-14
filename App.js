deabfc3f844c99b9d7d067ab0ed217ca05b4c96d
bitnick20 <65895288+bitnick20@users.noreply.github.com>
2020-08-14 18:35:22 +0200

    add react_flexbox1

--- App.js
+++ App.js
@@ -1,21 +1,60 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Flatlist, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+class App extends React.Component {
+  constructor (props) {
+    super(props);
+
+    this.state = {
+      countries: []
+    }
+  }
+componentDidMount () {
+  const url = 'http://restcountries.eu/rest/v2/all';
+  // console.log(url);
+
+  fetch(url)
+  .then(res => res.json())
+  .then(json => {
+
+    this.setState ({
+      countries: json
+    });
+  });
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  render () {
+    const {
+      countries
+    } = this.state
+
+    console.log('App/countries', countries)
+
+    return (
+      <View>
+        {/* <Flatlist /> */}
+        {countries.map((countrie => {
+          return (
+            <View style={{padding: 50}}>
+              <Text> {countrie.name}</Text>
+              <Text>{countrie.capital}</Text>
+            </View>
+            )
+        }))} 
+        <Text style={{fontSize: 40, paddingTop: 100, textAlign: 'center', color: 'red'}}>Hello Ali !</Text>
+      </View>
+    );
+  }
+}
+
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
+
+export default App;
